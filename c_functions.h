c942b9be907ea98a920251032930bd2b4ea804f7
Agi-Jer <127390932+Agi-Jer@users.noreply.github.com>
2024-11-26 18:13:00 +0800

    Header and Main C File

---
+++ c_functions.h
@@ -0,0 +1,18 @@
+#ifndef DFS_H
+#define DFS_H
+
+#include <stdio.h> 
+
+void malloc_vector(double* vector, int size);
+void input_vector(double* vector, int size);
+void print_vector(double* vector, int size);
+
+int get_start_time();
+int get_time_difference(int start_time);
+void print_time(int start_time);
+
+void call_c_kernel(double* x1, double* x2, double* y1, double* y2, double* z, int size);
+
+
+
+#endif 
